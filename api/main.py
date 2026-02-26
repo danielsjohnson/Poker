25feef75e6c112d90ff7715890181056df93e171
Danthecalfman <spikeeboy@outlook.com>
2026-02-25 20:23:26 -0800

    Add game state model and bot action endpoint

--- api/main.py
+++ api/main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
+
+
+class GameState(BaseModel):
+    hand: list[str]
+    community_cards: list[str]
+    pot_size: int
+    amount_to_call: int
+
+
+@app.get("/")
 def health_check():
-    return {"status": "The Poker Bot API is online and waiting for hands."}
+    return {"status": "ok"}
+
+@app.post("/get_action")
+def get_bot_action(state: GameState):
+    print(f"Recieved hand: {state.hand} with pot: {state.pot_size}")
+
+    return {
+        "bot_action": "Raise",
+        "explaination": "Dummy Response."
+    }
